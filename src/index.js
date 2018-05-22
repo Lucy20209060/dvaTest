84461e5c2505542046cf34c3555f51bc397e3a2d
luc <550502661@qq.com>
2018-05-23 00:14:30 +0800

    去掉url中的#号

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import dva from 'dva';
 import './index.css';
-import {browserHistory} from 'dva/router';
+import browserHistory from 'history/createBrowserHistory';
 // 1. Initialize
-const app = dva();
+const app = dva({
+    history: browserHistory()
+});
 
 // 2. Plugins
 app.use({});
