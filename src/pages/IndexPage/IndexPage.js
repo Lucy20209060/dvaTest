d137ad6442d2a0bb90477cde119d017d4dad01eb
luc <550502661@qq.com>
2018-05-27 23:23:57 +0800

    Link

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import { Link } from 'dva/router';
 
 function IndexPage() {
   return (
@@ -9,6 +10,7 @@ function IndexPage() {
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <Link to='/about'>about</Link>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
     </div>
